8abcee611ca95df600cdcc6ce014b017fa68dbb1
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-05-30 15:55:14 -0300

    (curso1-strategy): aplicação do strategy

--- design-pattern-001/projeto001/src/App.java
+++ design-pattern-001/projeto001/src/App.java
@@ -1,17 +1,24 @@
 import java.math.BigDecimal;
 
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
-import br.com.alura.loja.imposto.TipoImpostoEnum;
+import br.com.alura.loja.imposto.ICMS;
 import br.com.alura.loja.orcamento.Orcamento;
 
 public class App {
+
+    /**
+     * Strategy
+     * Sempre que temos um algoritmo que varia de acordo com um parâmetro ou informação
+     * 
+     */
+
     public static void main(String[] args) throws Exception {
         
         Orcamento orcamento = new Orcamento(new BigDecimal("100"));
 
         CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
 
-        System.out.println(calculadora.calcular(orcamento, TipoImpostoEnum.ISS));
+        System.out.println(calculadora.calcular(orcamento, new ICMS()));
 
     }
 }
